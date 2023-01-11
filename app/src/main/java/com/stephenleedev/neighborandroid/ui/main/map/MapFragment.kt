56756e5772d17f5a69f97d12e2771604e7ab6762
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-01-11 18:01:08 +0900

    Feat : NaverMap Feature Implementation

--- app/src/main/java/com/stephenleedev/neighborandroid/ui/main/map/MapFragment.kt
+++ app/src/main/java/com/stephenleedev/neighborandroid/ui/main/map/MapFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.stephenleedev.neighborandroid.R
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.stephenleedev.neighborandroid.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val binding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = false
     }
 
 }
