d68706c22b01b3cb69d00d9b8b689f540513fcb2
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-01-28 18:07:43 +0900

    Feat : Comma Adder Integer Extension Implementation

--- app/src/main/java/com/stephenleedev/neighborandroid/util/extension/IntExtension.kt
+++ app/src/main/java/com/stephenleedev/neighborandroid/util/extension/IntExtension.kt
@@ -1,6 +1,7 @@
 package com.stephenleedev.neighborandroid.util.extension
 
 import android.content.res.Resources
+import java.text.DecimalFormat
 
 /**
  * Written by StephenLeeDev on 2022/12/25.
@@ -8,3 +9,6 @@ import android.content.res.Resources
 
 val Int.toDp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+val Int.addCommas: String
+    get() = DecimalFormat("#,###,###,###,###").format(this)
