4e0d9b89bebab928202bc848c98e2b6cb8b0ab83
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-01-31 19:44:26 +0900

    Feat : ChatRoom List Feature Implementation

--- app/src/main/java/com/stephenleedev/neighborandroid/domain/model/request/post/RequestApplicationModel.kt
+++ app/src/main/java/com/stephenleedev/neighborandroid/domain/model/request/post/RequestApplicationModel.kt
@@ -1,19 +1,19 @@
 package com.stephenleedev.neighborandroid.domain.model.request.post
 
-
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import com.stephenleedev.neighborandroid.domain.model.chat.ChatMessageModel
 import com.stephenleedev.neighborandroid.domain.model.request.RequestModel
 import com.stephenleedev.neighborandroid.domain.model.user.UserModel
 
 data class RequestApplicationModel(
     @SerializedName("client")
-    @Expose
     val client: UserModel,
     @SerializedName("request")
-    @Expose
     val request: RequestModel,
+    @SerializedName("lastMessage")
+    val lastMessage: ChatMessageModel,
     @SerializedName("isAccepted")
-    @Expose
-    val isAccepted: Boolean
+    val isAccepted: Boolean,
+    @SerializedName("mapThumbnail")
+    val mapThumbnail: String
 )
